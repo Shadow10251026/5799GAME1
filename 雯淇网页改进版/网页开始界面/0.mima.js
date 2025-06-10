a0cce3834531d66ef2a899499ce1508d5621564d
Shadow10251026 <julianna.ling@qq.com>
2025-06-10 16:44:17 +0200

    Add files via upload

---
+++ 雯淇网页改进版/网页开始界面/0.mima.js
@@ -0,0 +1,10 @@
+loopy()   
+function loopy() {   
+var sWord =""  
+while (sWord != "10251026") {//设置密码 初始密码tzpr.cn
+sWord = prompt("欢迎来到围裙妈妈的小厨房，请输入邀请码!")   
+}   
+alert("欢迎光临o(^▽^)o")   
+}   
+
+
